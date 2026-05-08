1be8b79c19010660fc4468317a64e88a8bb29900
f7613656-pixel <f7613656@gmail.com>
2026-05-08 09:42:40 +0500

    ва

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -340,14 +340,7 @@ function App() {
 
             <nav className="navbar-container">
                 <div className="navbar">
-<button className={activeTab === 'inventory' ? 'active' : ''} onClick={() => handleTabChange('inventory')}>
-    <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-        <rect x="2" y="8" width="20" height="12" rx="2"></rect>
-        <line x1="2" y1="14" x2="22" y2="14"></line>
-    </svg>
-    <span>Инвентарь</span>
-</button>
+
                   
                     <button className={activeTab === 'home' ? 'active' : ''} onClick={() => handleTabChange('home')}>
     <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -355,6 +348,15 @@ function App() {
         <polyline points="9 22 9 12 15 12 15 22"></polyline>
     </svg>
     <span>Главная</span>
+</button>
+
+<button className={activeTab === 'inventory' ? 'active' : ''} onClick={() => handleTabChange('inventory')}>
+    <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+        <rect x="2" y="8" width="20" height="12" rx="2"></rect>
+        <line x1="2" y1="14" x2="22" y2="14"></line>
+    </svg>
+    <span>Инвентарь</span>
 </button>
 
 <button className={activeTab === 'shop' ? 'active' : ''} onClick={() => handleTabChange('shop')}>
